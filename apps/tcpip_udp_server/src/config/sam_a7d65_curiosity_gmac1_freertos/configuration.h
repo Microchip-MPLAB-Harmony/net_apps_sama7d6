cdf4fc68824d06953c1d39bc7a96743a718eff8f
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-10-09 15:25:31 +0530

    [MH3-99098] - Regenerated tcpip_udp_server application - sam_a7d65_curiosity_gmac1_freertos

--- apps/tcpip_udp_server/src/config/sam_a7d65_curiosity_gmac1_freertos/configuration.h
+++ apps/tcpip_udp_server/src/config/sam_a7d65_curiosity_gmac1_freertos/configuration.h
@@ -329,12 +329,6 @@ extern "C" {
 #define DRV_ETHPHY_LAN8840_RESET_CLR_TMO       500
 
 
-#define TCPIP_STACK_NETWORK_INTERAFCE_COUNT  	1
-
-
-
-
-
 /*** GMAC1 Configuration ***/
 #define DRV_GMAC1
 #define DRV_SAMA7D6
@@ -428,6 +422,12 @@ extern "C" {
 #define TCPIP_GMAC1_RX_PRIO_COUNT                1
 #define DRV_GMAC1_NUMBER_OF_QUEUES               6
 #define DRV_GMAC1_RMII_MODE                      0
+
+
+
+#define TCPIP_STACK_NETWORK_INTERAFCE_COUNT  	1
+
+
 
 
 
